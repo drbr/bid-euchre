eb9efc6d0e3f50b3fb0fff78837990946cffdfbd
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-21 13:25:38 -0800

    Format .ts files with Prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,15 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
 });
 
-export const makeUppercase = functions.database.ref('/messages/{pushId}/original')
-.onCreate((snapshot, context) => {
-  const original = snapshot.val();
-  console.log('Uppercasing', context.params.pushId, original);
-  const uppercase = original.toUpperCase();
-  return snapshot.ref.parent!.child('uppercase').set(uppercase);
-});
+export const makeUppercase = functions.database
+  .ref('/messages/{pushId}/original')
+  .onCreate((snapshot, context) => {
+    const original = snapshot.val();
+    console.log('Uppercasing', context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    return snapshot.ref.parent!.child('uppercase').set(uppercase);
+  });

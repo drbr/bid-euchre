eccc7acb1c9ae7979cd99c73c9be97d0cf6568a5
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-21 14:24:56 -0800

    Update NewGame function to correctly return the game ID

--- functions/src/newGame.ts
+++ functions/src/newGame.ts
@@ -1,7 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export function executeNewGame() {
+export type NewGameResult = {
+  gameId: string;
+};
+
+export function executeNewGame(): NewGameResult {
   const gameId = uuidv4();
   console.log(`New game ID: ${gameId}`);
-  return gameId;
+  return { gameId };
 }

e0e868eab691affac1c0f7f579922bdc2c6a9814
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-21 13:19:05 -0800

    Add another function to test with

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,11 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const makeUppercase = functions.database.ref('/messages/{pushId}/original')
+.onCreate((snapshot, context) => {
+  const original = snapshot.val();
+  console.log('Uppercasing', context.params.pushId, original);
+  const uppercase = original.toUpperCase();
+  return snapshot.ref.parent!.child('uppercase').set(uppercase);
+});

bf2522f37b5336642ae1db01b55b4f16237d43fc
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-21 18:39:51 -0800

    Add more lint rules to functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,9 +5,11 @@ module.exports = {
     "node": true
   },
   "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
-    "plugin:import/typescript"
+    "plugin:import/typescript",
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {

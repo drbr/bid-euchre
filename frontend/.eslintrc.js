fa9684333e4bcc253ceec678377dd9de68cf175f
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-22 14:07:05 -0800

    Upgrade typescript and eslint in both subrepos

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
   "rules": {
     "react/jsx-uses-react": "off",

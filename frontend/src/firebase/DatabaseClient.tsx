ac4128510df914e8e3cc7de9de7d895ce080bd81
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-22 23:43:46 -0800

    Move frontend database paths to file

--- frontend/src/firebase/DatabaseClient.tsx
+++ frontend/src/firebase/DatabaseClient.tsx
@@ -1,11 +1,12 @@
 import { PublicGameConfig } from '../../../functions/apiContract/database/DataModel';
 import { firebaseDatabase } from './FirebaseWebClientInFrontend';
+import * as DatabaseNodes from './DatabaseNodes';
 
 export async function getPublicGameConfig(
   gameId: string
 ): Promise<PublicGameConfig | null> {
   const snapshot = await firebaseDatabase
-    .ref(`publicGameConfig/${gameId}`)
+    .ref(DatabaseNodes.publicGameConfig(gameId))
     .get();
   return snapshot.val();
 }
---
+++ frontend/src/firebase/DatabaseNodes.tsx
@@ -0,0 +1,3 @@
+export function publicGameConfig(gameId: string) {
+  return `publicGameConfig/${gameId}`;
+}

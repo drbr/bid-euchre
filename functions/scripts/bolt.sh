148607d96993c667e407244c9ed91451e44550e2
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-22 21:18:55 -0800

    Maintain TS database schema by hand

--- functions/scripts/bolt.sh
+++ functions/scripts/bolt.sh
@@ -6,25 +6,10 @@ script_dir=${0:h}
 schemaFile="../databaseSchema.bolt"
 
 databaseRulesJsonOutput="../database.rules.json"
-typescriptSchemaOutput="./apiContract/database/Schema.ts"
-
-typescriptSchemaHeader='/*
- * THIS IS AN AUTO-GENERATED FILE – DO NOT EDIT BY HAND!
- *
- * To update the schema types, modify `databaseSchema.bolt` and then run `yarn bolt`
- * to regenerate this file.
- */
-'
 
 # Clean up the old files to be extra sure
 rm "$databaseRulesJsonOutput" 2>/dev/null
-rm "$typescriptSchemaOutput" 2>/dev/null
 
 
 # Compile the JSON database rules schema
 firebase-bolt < "$schemaFile" > "$databaseRulesJsonOutput"
-
-
-# Compile the Typescript typedefs
-echo "$typescriptSchemaHeader" > "$typescriptSchemaOutput"
-firebase-bolt-compiler < "$schemaFile" >> "$typescriptSchemaOutput"

dec2394323b3f67970dec0da1e987b39b2f5bf1f
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-23 02:22:56 -0800

    Subscribe to game value from frontend

--- frontend/src/firebase/ReadDAO.tsx
+++ frontend/src/firebase/ReadDAO.tsx
@@ -1,6 +1,8 @@
 import { PublicGameConfig } from '../../../functions/apiContract/database/DataModel';
 import { firebaseDatabase } from './FirebaseWebClientInFrontend';
 
+export type UnsubscribeFn = () => void;
+
 export async function getPublicGameConfig(
   gameId: string
 ): Promise<PublicGameConfig | null> {
@@ -8,4 +10,15 @@ export async function getPublicGameConfig(
     .ref(`/publicGameConfig/${gameId}`)
     .get();
   return snapshot.val();
+}
+
+export function subscribeToPublicGameConfig(
+  gameId: string,
+  callback: (gameConfig: PublicGameConfig) => void
+): UnsubscribeFn {
+  const ref = firebaseDatabase.ref(`/publicGameConfig/${gameId}`);
+  const unsubscribeKey = ref.on('value', (snapshot) =>
+    callback(snapshot.val())
+  );
+  return () => ref.off('value', unsubscribeKey);
 }
--- frontend/src/screens/GameContainer.tsx
+++ frontend/src/screens/GameContainer.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from '@reach/router';
 import { useEffect, useState } from 'react';
 import * as DAO from '../firebase/ReadDAO';
 import { GameNotFound } from './GameNotFound';
@@ -9,17 +8,16 @@ export type GameContainerProps = {
 
 export function GameContainer(props: GameContainerProps) {
   const [gameValue, setGameValue] = useState<string | undefined>(undefined);
-  const navigate = useNavigate();
 
   useEffect(() => {
-    async function fetchGameValue() {
-      const game = await DAO.getPublicGameConfig(props.gameId);
-      setGameValue(game ? String(JSON.stringify(game)) : '');
-    }
-    void fetchGameValue();
-  }, [props.gameId, navigate]);
+    return DAO.subscribeToPublicGameConfig(props.gameId, (gameConfig) => {
+      const gameValue = gameConfig ? String(JSON.stringify(gameConfig)) : '';
+      setGameValue(gameValue);
+    });
+  }, [props.gameId]);
 
   if (gameValue === undefined) {
+    // Still loading the initial value
     return <></>;
   }
 

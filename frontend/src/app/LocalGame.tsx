904eec426db9d87e77a7e976a0e0e9c6866f8ff5
Andrew Brandon <drbr@users.noreply.github.com>
2020-11-27 20:26:54 -0800

    Wire up local game

--- frontend/src/app/LocalGame.tsx
+++ frontend/src/app/LocalGame.tsx
@@ -1,4 +1,11 @@
+import { useMachine } from '@xstate/react';
+import { PublicGameConfig } from '../../../functions/apiContract/database/DataModel';
+import { GameStateMachine } from '../gameLogic/stateMachine/GameStateMachine';
+import { GameDisplay } from '../gameScreens/GameDisplay';
+
 export function LocalGame() {
+  const [state, send] = useMachine(GameStateMachine);
+
   return (
     <div>
       <h1>Local Game</h1>
@@ -6,6 +13,23 @@ export function LocalGame() {
         Use this to develop and test the game UI locally, without involving the
         server.
       </p>
+      <GameDisplay
+        machineState={state}
+        machineContext={state.context}
+        sendGameEvent={send}
+        gameConfig={DummyGameConfig}
+        seatedAt="east"
+      />
     </div>
   );
 }
+
+const DummyGameConfig: PublicGameConfig = {
+  gameStatus: 'inProgress',
+  playerFriendlyNames: {
+    north: 'Nancy',
+    west: 'William',
+    south: 'Susan',
+    east: 'Edward',
+  },
+};

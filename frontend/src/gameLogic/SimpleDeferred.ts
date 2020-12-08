ee09e37c250e12eb0a327b520c991b4818bbd5d3
Andrew Brandon <drbr@users.noreply.github.com>
2020-12-07 21:24:57 -0800

    Fix SimpleDeferred

--- frontend/src/gameLogic/SimpleDeferred.ts
+++ frontend/src/gameLogic/SimpleDeferred.ts
@@ -2,19 +2,16 @@
  * An implementation of the "deferred" pattern, as seen in many promise libraries.
  */
 export class SimpleDeferred<TResult> {
-  private _promise: Promise<TResult>;
+  public readonly promise: Promise<TResult>;
+
   private _resolve!: (result: TResult | PromiseLike<TResult>) => void;
   private _reject!: (reason: unknown) => void;
 
   constructor() {
-    this._promise = new Promise<TResult>((resolve, reject) => {
+    this.promise = new Promise<TResult>((resolve, reject) => {
       this._resolve = resolve;
       this._reject = reject;
     });
-  }
-
-  public get promise() {
-    return this._promise;
   }
 
   public get resolve() {

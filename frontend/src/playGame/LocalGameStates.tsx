0df6186992d903113a5ac57f9691c92495d3cdf1
Andrew Brandon <drbr@users.noreply.github.com>
2021-02-06 17:34:48 -0800

    Start local game at "Played One Card"

--- frontend/src/playGame/LocalGameStates.tsx
+++ frontend/src/playGame/LocalGameStates.tsx
@@ -277,3 +277,151 @@ export const NameTrump = JSON.parse(`
     "position": "north"
   }
 }`);
+
+export const PlayedOneCard = JSON.parse(`
+{
+  "value": {
+    "runGame": {
+      "round": {
+        "thePlay": {
+          "trick": "waitForFollow"
+        }
+      }
+    }
+  },
+  "actions": [],
+  "event": {
+    "type": "PLAY_CARD",
+    "position": "south",
+    "card": {
+      "rank": "10",
+      "suit": "H"
+    }
+  },
+  "_event": {
+    "name": "PLAY_CARD",
+    "data": {
+      "type": "PLAY_CARD",
+      "position": "south",
+      "card": {
+        "rank": "10",
+        "suit": "H"
+      }
+    },
+    "$$type": "scxml",
+    "type": "external"
+  },
+  "context": {
+    "score": {
+      "eastwest": 0,
+      "northsouth": 0
+    },
+    "eventCount": 1,
+    "previousEventCount": 0,
+    "currentDealer": "north",
+    "private_hands": {
+      "north": [{
+        "rank": "K",
+        "suit": "S"
+      }, {
+        "rank": "A",
+        "suit": "D"
+      }, {
+        "rank": "K",
+        "suit": "D"
+      }, {
+        "rank": "J",
+        "suit": "D"
+      }, {
+        "rank": "9",
+        "suit": "S"
+      }, {
+        "rank": "10",
+        "suit": "S"
+      }],
+      "south": [{
+        "rank": "9",
+        "suit": "H"
+      }, {
+        "rank": "K",
+        "suit": "C"
+      }, {
+        "rank": "10",
+        "suit": "C"
+      }, {
+        "rank": "J",
+        "suit": "H"
+      }, {
+        "rank": "Q",
+        "suit": "H"
+      }],
+      "east": [{
+        "rank": "10",
+        "suit": "D"
+      }, {
+        "rank": "9",
+        "suit": "D"
+      }, {
+        "rank": "J",
+        "suit": "S"
+      }, {
+        "rank": "Q",
+        "suit": "C"
+      }, {
+        "rank": "Q",
+        "suit": "S"
+      }, {
+        "rank": "J",
+        "suit": "C"
+      }],
+      "west": [{
+        "rank": "A",
+        "suit": "H"
+      }, {
+        "rank": "Q",
+        "suit": "D"
+      }, {
+        "rank": "9",
+        "suit": "C"
+      }, {
+        "rank": "A",
+        "suit": "C"
+      }, {
+        "rank": "K",
+        "suit": "H"
+      }, {
+        "rank": "A",
+        "suit": "S"
+      }]
+    },
+    "awaitedPlayer": "west",
+    "bids": {
+      "north": "pass",
+      "south": 3,
+      "east": 2,
+      "west": "pass"
+    },
+    "trump": "H",
+    "highestBid": 3,
+    "highestBidder": "south",
+    "playersSittingOut": [],
+    "leader": "south",
+    "currentTrick": {
+      "north": null,
+      "south": {
+        "rank": "10",
+        "suit": "H"
+      },
+      "east": null,
+      "west": null
+    },
+    "trickCount": {
+      "north": 0,
+      "south": 0,
+      "east": 0,
+      "west": 0
+    }
+  },
+  "meta": {}
+}
+`);
